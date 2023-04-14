3a81d11a062f5e4d8b9025c50d848fa0bbb651b6
karen lee <hjlee8820@gmail.com>
2023-04-14 15:15:54 -0500

    add email column to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_161549) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_14_201049) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -23,5 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_161549) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
+
 end
